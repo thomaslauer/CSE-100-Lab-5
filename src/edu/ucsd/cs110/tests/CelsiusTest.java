078b65c4d81dc4d2733106def550b552ca0df8b7
Thomas Lauer <tlauer@ucsd.edu>
2019-05-01 11:06:46 -0700

    added entire CelsiusTest file

--- src/edu/ucsd/cs110/tests/CelsiusTest.java
+++ src/edu/ucsd/cs110/tests/CelsiusTest.java
@@ -6,14 +6,14 @@ import junit.framework.TestCase;
 public class CelsiusTest extends TestCase {
     private float delta = 0.001f;
 
-    public void testCelsius() {
+    public void testCelsius(){
         float value = 12.34f;
         Celsius temp = new Celsius(value);
 
         assertEquals(value, temp.getValue(), delta);
     }
 
-    public void testCelsiusToString() {
+    public void testCelsiusToString(){
         float value = 12.34f;
 
         Celsius temp = new Celsius(value);
@@ -29,5 +29,28 @@ public class CelsiusTest extends TestCase {
         assertTrue(string.endsWith(ending));
 
         // Verify the middle of the formatted string
+        int endIndex = string.indexOf(ending);
+
+        // (Hint: what is the length of the middle of the string?)
+        assertTrue(string.substring(0, endIndex).equals(beginning));
+    }
+
+    public void testCelsiusToCelsius()
+    {
+        Celsius temp = new Celsius(0);
+        Temperature convert = temp.toCelsius();
+        assertEquals(0, convert.getValue(), delta);
+    }
+
+    public void testCelsiusToFahrenheit(){
+        Celsius temp = new Celsius(0);
+
+        Temperature convert = temp.toFahrenheit();
+        assertEquals(32, convert.getValue(), delta);
+
+        temp = new Celsius(100);
+        convert = temp.toFahrenheit();
+
+        assertEquals(212, convert.getValue(), delta);
     }
 }
